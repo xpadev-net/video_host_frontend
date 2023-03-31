11653c34e46b76b54915eedb31aaced482c9fa15
xpa <xpadev@gmail.com>
2023-04-01 03:15:28 +0900

    feat: movie info display

--- components/MovieInfo/MovieInfo.tsx
+++ components/MovieInfo/MovieInfo.tsx
@@ -10,7 +10,12 @@ type props = {
 const MovieInfo = ({ data, className }: props) => {
   return (
     <div className={`${className} ${Styles.wrapper}`}>
-      <div>{data.movie.title}</div>
+      <div className={Styles.titleWrapper}>
+        <div className={Styles.title}>{data.movie.title}</div>
+        <div className={Styles.postDate}>
+          {new Date(data.movie.addAt * 1000).toLocaleDateString()}
+        </div>
+      </div>
       <Link href={`/series/${data.movie.seriesUrl}`}>
         {data.movie.seriesTitle}
       </Link>
