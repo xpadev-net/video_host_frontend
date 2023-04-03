468c5a657b47f219c63913f182a8937b08eeb1f9
xpa <xpadev@gmail.com>
2023-04-03 12:39:46 +0900

    improve:split component

---
+++ components/App/Header/DesktopHeader.tsx
@@ -0,0 +1,26 @@
+import Styles from "@/components/App/Header/Header.module.scss";
+import { HeaderMenu } from "@/components/App/Menu/Menu";
+import { Logout } from "@/components/App/Header/Logout/Logout";
+import { Search } from "@/components/App/Header/Search/Search";
+
+type props = {
+  className?: string;
+};
+
+const DesktopHeader = ({ className }: props) => {
+  return (
+    <header className={`${className} ${Styles.container}`}>
+      <div className={Styles.leftSideWrapper}>
+        <HeaderMenu />
+      </div>
+      <div className={Styles.centerWrapper}>
+        <Search />
+      </div>
+      <div className={Styles.rightSideWrapper}>
+        <Logout />
+      </div>
+    </header>
+  );
+};
+
+export { DesktopHeader };
--- components/App/Header/Header.tsx
+++ components/App/Header/Header.tsx
@@ -1,9 +1,6 @@
-import Styles from "@/components/App/Header/Header.module.scss";
-import { HeaderMenu } from "@/components/App/Menu/Menu";
-import { Logout } from "@/components/App/Header/Logout/Logout";
-import { Search } from "@/components/App/Header/Search/Search";
 import { useIsMobile } from "@/libraries/isMobile";
 import { MobileHeader } from "@/components/App/Header/MobileHeader";
+import { DesktopHeader } from "@/components/App/Header/DesktopHeader";
 
 type props = {
   className?: string;
@@ -12,19 +9,7 @@ type props = {
 const Header = ({ className }: props) => {
   const isMobile = useIsMobile();
   if (isMobile) return <MobileHeader className={className} />;
-  return (
-    <header className={`${className} ${Styles.container}`}>
-      <div className={Styles.leftSideWrapper}>
-        <HeaderMenu />
-      </div>
-      <div className={Styles.centerWrapper}>
-        <Search />
-      </div>
-      <div className={Styles.rightSideWrapper}>
-        <Logout />
-      </div>
-    </header>
-  );
+  return <DesktopHeader />;
 };
 
 export { Header };
