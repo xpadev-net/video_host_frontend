1615ae9c9e5837326f50f9758b1b0ba306bbb93d
xpadev <xpadev@gmail.com>
2025-01-25 19:56:39 +0900

    refactor: auth

--- src/atoms/Auth.ts
+++ src/atoms/Auth.ts
@@ -2,4 +2,10 @@ import { atom } from "jotai";
 import { atomWithStorage } from "jotai/utils";
 
 export const AuthModalOpenAtom = atom<boolean>(false);
-export const AuthTokenAtom = atomWithStorage<string | null>("token", null);
+
+export const AuthTokenLocalStorageKey = "token";
+
+export const AuthTokenAtom = atomWithStorage<string | null>(
+  AuthTokenLocalStorageKey,
+  null,
+);
--- src/libraries/requests.ts
+++ src/libraries/requests.ts
@@ -1,10 +1,17 @@
 import axios from "axios";
 
+import { AuthTokenLocalStorageKey } from "@/atoms/Auth";
 import { ApiEndpoint } from "@/contexts/env";
+
+const token =
+  typeof window == "undefined"
+    ? null
+    : localStorage.getItem(AuthTokenLocalStorageKey);
 
 export const requests = axios.create({
   baseURL: ApiEndpoint,
   headers: {
     "Content-Type": "application/json",
+    Authorization: token ? `Bearer ${token.slice(1, -1)}` : undefined,
   },
 });
