f0d43b4ba3a6a3ec332614f4ace8f198b5fede88
xpadev <xpadev@gmail.com>
2025-04-21 16:32:47 +0900

    chore: refactor docker build process

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-./node_modules/next/dist/bin/next build;./node_modules/next/dist/bin/next start
+(npx prisma generate > /dev/null)&&(npx prisma migrate deploy)&&./node_modules/next/dist/bin/next start
