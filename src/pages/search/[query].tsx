aa101795c92367270039014f6afe8e46fd26f5ad
xpadev <xpadev@gmail.com>
2025-08-19 20:43:16 +0900

    refactor: replace Tabs component with TabSwitcher for improved search page UI

--- src/pages/search/[query].tsx
+++ src/pages/search/[query].tsx
@@ -1,34 +1,35 @@
-import { Tabs } from "@radix-ui/themes";
 import Head from "next/head";
 import { useRouter } from "next/router";
 
 import { MoviesSearchList } from "@/components/SearchList/MoviesSearchList";
 import { SeriesSearchList } from "@/components/SearchList/SeriesSearchList";
+import { type TabItem, TabSwitcher } from "@/components/TabSwitcher";
 import { SiteName } from "@/contexts/env";
 import Styles from "@/styles/search.module.scss";
 
 const SearchPage = () => {
   const router = useRouter();
   const query = router.query.query;
+
+  const tabs: TabItem[] = [
+    {
+      value: "series",
+      label: "シリーズ",
+      content: <SeriesSearchList query={query as string} />,
+    },
+    {
+      value: "movies",
+      label: "動画",
+      content: <MoviesSearchList query={query as string} />,
+    },
+  ];
+
   return (
     <div className={Styles.wrapper}>
       <Head>
         <title>{`検索: ${query} - ${SiteName}`}</title>
       </Head>
-      <h1>{`検索: ${query}`}</h1>
-      <Tabs.Root defaultValue={"series"} className={Styles.tab}>
-        <Tabs.List className={Styles.list}>
-          <Tabs.Trigger value="series">シリーズ</Tabs.Trigger>
-          <Tabs.Trigger value="movies">動画</Tabs.Trigger>
-        </Tabs.List>
-
-        <Tabs.Content value="series">
-          <SeriesSearchList query={`${query}`} />
-        </Tabs.Content>
-        <Tabs.Content value="movies">
-          <MoviesSearchList query={`${query}`} />
-        </Tabs.Content>
-      </Tabs.Root>
+      <TabSwitcher tabs={tabs} defaultValue="series" />
     </div>
   );
 };
