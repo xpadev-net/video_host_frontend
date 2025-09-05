522356a1d346fc3310defe5eb2dfb551721b47f7
xpadev <xpadev@gmail.com>
2025-09-06 04:15:51 +0900

    refactor: remove unused styles and update HeaderMenu component with new button implementation

--- src/components/App/Menu/Menu.tsx
+++ src/components/App/Menu/Menu.tsx
@@ -1,11 +1,10 @@
 import { useSetAtom } from "jotai";
+import { Menu, Tv } from "lucide-react";
 import Link from "next/link";
-import { MdMenu, MdOndemandVideo } from "react-icons/md";
-
 import { sidebarState } from "@/atoms/SidebarState";
-import Styles from "@/components/App/Menu/Menu.module.scss";
+import { Button } from "@/components/ui/button";
+import { SiteName } from "@/contexts/env";
 import { useIsMobile } from "@/libraries/isMobile";
-import ButtonStyles from "@/styles/button.module.scss";
 
 const HeaderMenu = () => {
   const setIsSidebarActive = useSetAtom(sidebarState);
@@ -14,22 +13,28 @@ const HeaderMenu = () => {
     setIsSidebarActive((pv) => !pv);
   };
   return (
-    <div className={Styles.wrapper}>
+    <div className={"flex flex-row gap-2"}>
       {!isMobile && (
-        <button
-          type="button"
-          className={`${ButtonStyles.buttonWrapper} ${ButtonStyles.hover}`}
+        <Button
           onClick={toggleSidebar}
           aria-label="Toggle sidebar"
+          type="button"
+          variant={"ghost"}
+          className="w-10 h-10 p-0 cursor-pointer"
         >
-          <MdMenu />
-        </button>
+          <Menu />
+        </Button>
       )}
-      <Link href={"/"} className={Styles.homeButton}>
-        <div className={ButtonStyles.buttonWrapper}>
-          <MdOndemandVideo />
-        </div>
-      </Link>
+      <Button
+        variant={"ghost"}
+        className="cursor-pointer text-left h-10"
+        asChild
+      >
+        <Link href={"/"}>
+          <Tv />
+          {SiteName}
+        </Link>
+      </Button>
     </div>
   );
 };
