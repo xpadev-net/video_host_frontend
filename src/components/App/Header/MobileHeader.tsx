c87166d7a6164c9f4d7e28778c6d0bf68f247471
xpadev <xpadev@gmail.com>
2025-09-16 15:52:57 +0900

    refactor: remove Header.module.scss and apply utility classes in DesktopHeader and MobileHeader components

--- src/components/App/Header/MobileHeader.tsx
+++ src/components/App/Header/MobileHeader.tsx
@@ -2,7 +2,6 @@ import { ArrowLeft, SearchIcon } from "lucide-react";
 import { useRouter } from "next/router";
 import { useEffect, useRef, useState } from "react";
 import { AuthButton } from "src/components/App/Header/Auth";
-import Styles from "@/components/App/Header/Header.module.scss";
 import { Search } from "@/components/App/Header/Search";
 import { ToggleTheme } from "@/components/App/Header/Theme";
 import { HeaderMenu } from "@/components/App/Menu";
@@ -28,11 +27,13 @@ const MobileHeader = ({ className }: props) => {
   const closeSearch = () => setIsInputActive(false);
 
   return (
-    <header className={`${className} ${Styles.container}`}>
-      <div className={Styles.leftSideWrapper}>
+    <header
+      className={`${className} px-4 flex flex-row items-center justify-between`}
+    >
+      <div className="w-[169px]">
         <HeaderMenu />
       </div>
-      <div className={Styles.rightSideWrapper}>
+      <div className="w-[225px] flex justify-end gap-1">
         <Button variant="ghost" size="icon" onClick={onSearchButtonClick}>
           <SearchIcon size={16} />
         </Button>
@@ -40,16 +41,18 @@ const MobileHeader = ({ className }: props) => {
         <AuthButton />
       </div>
       <div
-        className={`${Styles.inputWrapper} ${isInputActive && Styles.active}`}
+        className={`absolute left-0 top-0 h-full w-full flex bg-[var(--color-background)] px-2 opacity-0 pointer-events-none z-[1] items-center ${
+          isInputActive ? "opacity-100 pointer-events-auto" : ""
+        }`}
       >
         <Button variant="ghost" onClick={closeSearch} size={"icon"}>
           <ArrowLeft />
         </Button>
-        <Search className={Styles.input} ref={inputRef} />
+        <Search className="flex-1 my-1" ref={inputRef} />
       </div>
       {isInputActive && (
         <button
-          className={Styles.background}
+          className="fixed z-[-1] top-0 left-0 w-screen h-screen bg-[rgba(0,0,0,0.3)]"
           onClick={closeSearch}
           onKeyDown={(e) => {
             if (e.key === "Enter" || e.key === " ") {
