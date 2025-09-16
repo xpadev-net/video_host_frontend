f6d33b2128514ca2e9c7e09ea55677bb601c3137
xpadev <xpadev@gmail.com>
2025-09-16 16:00:22 +0900

    refactor: remove MovieInfo.module.scss and apply utility classes in MovieInfo and MovieInfoSkeleton components

--- src/components/MovieInfo/MovieInfo.tsx
+++ src/components/MovieInfo/MovieInfo.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
 
 import type { FilteredMovie } from "@/@types/v4Api";
-import Styles from "@/components/MovieInfo/MovieInfo.module.scss";
 import { User } from "@/components/User/User";
+import { cn } from "@/lib/utils";
 
 type props = {
   data: FilteredMovie;
@@ -11,19 +11,22 @@ type props = {
 
 const MovieInfo = ({ data, className }: props) => {
   return (
-    <div className={`${className} ${Styles.wrapper}`}>
-      <div className={Styles.titleWrapper}>
-        <div className={Styles.title}>{data.title}</div>
-        <div className={Styles.postDate}>
+    <div className={cn(className)}>
+      <div className="border-b border-[var(--color-quaternary-background)] mb-2.5">
+        <div className="text-[18px]">{data.title}</div>
+        <div className="text-[14px] text-[var(--color-sub-text)] mb-2.5">
           {new Date(data.createdAt).toLocaleDateString()}
         </div>
       </div>
-      <div className={Styles.author}>
+      <div className="pb-2.5 border-b border-[var(--color-quaternary-background)] mb-2.5 flex flex-row gap-2 items-center">
         <User user={data.author} />
       </div>
-      <div className={Styles.series}>
+      <div className="text-[14px] pb-2.5 leading-5">
         <p>この動画を含むプレイリスト</p>
-        <Link className={Styles.link} href={`/series/${data.series?.id}`}>
+        <Link
+          className="text-[var(--color-sub-text)] no-underline hover:underline"
+          href={`/series/${data.series?.id}`}
+        >
           {data.series?.title}
         </Link>
       </div>
